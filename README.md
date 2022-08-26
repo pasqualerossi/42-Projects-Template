3d2c9cc7c098e52de1b0e70c0290d2b0e616b8af
Pasquale Rossi <prossi@F2N072.42adel.org.au>
2022-08-26 16:24:25 +0930

    cleaned sh and added copyable code lines to README

--- README.md
+++ README.md
@@ -2,14 +2,14 @@
 
 Copy the following and paste it in your iTerm.
 
-```
-cd Desktop && git clone 
+```sh
+cd ~/Desktop; git clone https://github.com/pasqualerossi/42-Folder-Structure.git 42fs; 42fs/gen.sh 42fs/piscine.fs
 ```
 
 # For 42 School Students
 
 Copy the following and paste it in your iTerm.
 
-```
-
+```sh
+cd ~/Desktop; git clone https://github.com/pasqualerossi/42-Folder-Structure.git 42fs; 42fs/gen.sh 42fs/school.fs
 ```
--- gen.sh
+++ gen.sh
@@ -1,30 +1,25 @@
-generateStructure()
-{
-	prevtab=0
-	prevfolder=""
-	IFS=''
-	while read line || [ -n "$line" ]
-	do
-		currtab=$(echo $line | grep -Eo "(\t|    )" | wc -l | grep -Eo "\d+")
-		currfolder=$(echo $line | grep -Eo "[^	 ]+")
+prevtab=0
+prevfolder=""
+IFS=''
+while read line || [ -n "$line" ]
+do
+	currtab=$(echo $line | grep -Eo "(\t|    )" | wc -l | grep -Eo "\d+")
+	currfolder=$(echo $line | grep -Eo "[^	 ]+")
 
-		if [ $currtab -gt $prevtab ]
-		then
-			cd $prevfolder
-		elif [ $prevtab -gt $currtab ]
-		then
-			while [ $prevtab -gt $currtab ]
-			do
-				cd ..
-				let "prevtab=$prevtab-1"
-			done
-		fi
+	if [ $currtab -gt $prevtab ]
+	then
+		cd $prevfolder
+	elif [ $prevtab -gt $currtab ]
+	then
+		while [ $prevtab -gt $currtab ]
+		do
+			cd ..
+			let "prevtab=$prevtab-1"
+		done
+	fi
 
-		mkdir $currfolder
+	mkdir $currfolder
 
-		prevtab=$currtab
-		prevfolder=$currfolder
-	done < "$1"
-}
-
-generateStructure structure.fs
+	prevtab=$currtab
+	prevfolder=$currfolder
+done < "$1"
